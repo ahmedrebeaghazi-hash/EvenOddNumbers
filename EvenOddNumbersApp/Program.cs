7cd000072478f718074ed8aab5cd87456ec37dc2
ahmedrebeaghazi-hash <ahmedrebeaghazi@gmail.com>
2025-12-10 18:59:47 +0200

    initial commit

---
+++ EvenOddNumbersApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+namespace EvenOddNumbersApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            PrintNumbers("Numbers :", Numbers.Where(x => Even(x)));
+
+            PrintNumbers("Numbers :", Numbers.Where(x => Odd(x)));
+
+
+        }
+        static void PrintNumbers(string title, IEnumerable<int> numbers)
+        {
+            Console.WriteLine(title);
+            foreach (int n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+        }
+        static bool Even(int Number)
+        {
+            if(Number > 0 && Number %2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static bool Odd(int Number)
+        {
+            if (Number > 0 && Number % 2 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
